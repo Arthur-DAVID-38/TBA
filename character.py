f3398f20f7e07a421343caf6d91ea5d60704b846
mohamed-amine.rguigue <mohamed-amine.rguigue@edu.esiee.fr>
2026-01-21 09:44:20 +0000

    Ajout de docstrings

--- character.py
+++ character.py
@@ -1,12 +1,18 @@
+"""Définit la classe Character utilisée pour les PNJ du jeu TBA."""
+
+
 class Character:
+    """Représente un personnage non-joueur avec des messages cycliques."""
 
     def __init__(self, name, description, messages):
+        """Initialise un personnage avec son nom, sa description et ses messages."""
         self.name = name
         self.description = description
         self.messages = messages
         self.index = 0
 
     def get_msg(self):
+        """Retourne le message courant du personnage et avance le dialogue."""
         if not self.messages:
             return f"{self.name} ne dit rien."
         msg = self.messages[self.index]
@@ -14,4 +20,5 @@ class Character:
         return msg
 
     def __str__(self):
+        """Retourne la représentation textuelle du personnage."""
         return f"{self.name} : {self.description}"
