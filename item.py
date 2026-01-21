532abd8c1d5b5b4f64e64f1273c8ff71dd03ed03
mohamed-amine.rguigue <mohamed-amine.rguigue@edu.esiee.fr>
2026-01-21 09:57:27 +0000

    Ajout de docstring, suppression ligne vide, ajout d'une deuxieme méthode publique pour éviter le too few public methods de pylint

--- item.py
+++ item.py
@@ -1,12 +1,29 @@
+"""Définition de la classe Item pour les objets du jeu."""
+
+
 class Item:
-    """Objet du jeu"""
+    """Objet manipulable dans le jeu.
+
+    Attributes:
+        name (str): Nom affiché de l'objet.
+        description (str): Description textuelle de l'objet.
+        weight (int | float): Poids de l'objet, utilisé pour la capacité de port.
+    """
 
     def __init__(self, name, description, weight):
+        """Initialise un nouvel objet de jeu."""
         self.name = name
         self.description = description
         self.weight = weight
 
     def __str__(self):
+        """Retourne une représentation textuelle lisible de l'objet."""
         return f"{self.name} : {self.description} ({self.weight} kg)"
 
-
+    def to_dict(self):
+        """Retourne une représentation simple de l'objet sous forme de dictionnaire."""
+        return {
+            "name": self.name,
+            "description": self.description,
+            "weight": self.weight,
+        }
